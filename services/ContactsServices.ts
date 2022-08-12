717bb87a6418dd674815baa23f265b27b6c2874b
Esteban Gutierrez <esteb.gtz.mtz@hotmail.com>
2022-08-12 03:08:03 -0500

    fixing serveices

--- services/ContactsServices.ts
+++ services/ContactsServices.ts
@@ -1,7 +1,17 @@
 import axios from 'axios';
+import { IDataFromContacts, IDataFullResponse, INewContactSubmit, IUserByID } from '../interfaces/interfaces';
 
 export const baseURL: string = 'https://bkbnchallenge.herokuapp.com';
 
 export const service = axios.create({
   baseURL
 });
+
+export const createNewContact = async (newContact:INewContactSubmit) => {
+  try {
+    const {data}:IDataFullResponse = await service.post(`${baseURL}/contacts`, newContact);
+    return data;
+  } catch (error:any) {
+    return error.response.data.message
+  }
+}
