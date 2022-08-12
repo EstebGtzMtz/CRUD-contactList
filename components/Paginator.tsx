3a8ffc74f2b4ac6e9aacddcb60c901edf9629a04
esteb.gtz.mtz <esteb.gtz.mtz@hotmail.com>
2022-08-12 15:49:21 -0500

    solve pagination error

--- components/Paginator.tsx
+++ components/Paginator.tsx
@@ -2,19 +2,24 @@ import { Pagination, Stack } from '@mui/material'
 import { useDispatch, useSelector } from 'react-redux'
 import { IContactsState } from '../interfaces/interfaces'
 import { getAllContacts } from '../redux/actions'
+import { useState } from 'react';
 
 const Paginator = () => {
   const dispatch = useDispatch()
-  const currentPage = useSelector((state:IContactsState) => state?.contacts.currentPage);
+  let currentPage = useSelector((state:IContactsState) => state?.contacts.currentPage);
   const totalPages = useSelector((state:IContactsState) => state?.contacts.totalPages);
+
+  const [page, setPage] = useState(currentPage);
+  
 
   const getContactsByPage = async (event: React.ChangeEvent<unknown>, page: number) => {
     dispatch(getAllContacts(page))
+    setPage(page)
   }
 
   return (
     <Stack spacing={2}>
-      <Pagination count={totalPages ? totalPages : 1} defaultPage={currentPage ? currentPage : 1} variant="outlined" color="secondary" size='large' onChange={getContactsByPage}/>
+      <Pagination count={totalPages ? totalPages : 1} page={page} variant="outlined" color="secondary" onChange={getContactsByPage}/>
     </Stack>
   )
 }
